536b23efb24509b8e91f6412e87c3b3ed4ef5d89
jmcph4 <jack@jmcph4.dev>
2025-03-18 13:45:06 +1000

    Emit event

--- wasm-runner/contracts/src/BytecodeRegistry.sol
+++ wasm-runner/contracts/src/BytecodeRegistry.sol
@@ -2,12 +2,15 @@
 pragma solidity 0.8.29;
 
 contract BytecodeRegistry {
+    event ExecutionRequest(address sender, bytes code);
+
     uint256 public numPrograms;
     mapping (uint256 => bytes) public programs;
 
     function requestExecution(bytes calldata code) public returns (uint256) {
         programs[numPrograms] = code;
         numPrograms += 1;
+        emit ExecutionRequest(msg.sender, code);
         return numPrograms - 1;
     }
 }
